74588e233a0f8d08e08098ff3c5e9feb40e18504
Sergi Guzman <sergi.guzman@glovoapp.com>
2025-01-04 01:50:10 +0100

    chore: test linter

--- src/controller/editor-history/index.ts
+++ src/controller/editor-history/index.ts
@@ -28,7 +28,7 @@ export class EditorHistory {
     return this.#entries
   }
 
-  private createEntry(action: string, icon: ReactElement) {
+  private createEntry(action: string, icon: ReactElement, test: any) {
     // create history data
     const arrayBuffer = new ArrayBuffer(
       this.#dependencies.image.size.w * this.#dependencies.image.size.h * 4
