940458c0d76419b3a7c86f7d3a5d8051126e3c18
Sergi Guzman <sergi.guzman@glovoapp.com>
2025-01-05 03:38:47 +0100

    feat: shortcuts controller

--- jest.config.js
+++ jest.config.js
@@ -8,14 +8,11 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  rootDir: 'src',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.ts?$': 'ts-jest',
   },
-  // rootDir: 'src',
-
-  rootDir: 'src',
-
   moduleNameMapper: {
     //   // '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
     '^@/(.*)$': '<rootDir>/src/$1',
